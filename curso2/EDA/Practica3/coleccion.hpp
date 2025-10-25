4011ba8088aaac8c55dbc8ea555054dec3458ae9
EEEDU <edu7guerrero@gmail.com>
2025-10-25 18:36:05 +0200

    EDA - continuacion practica 3

--- curso2/EDA/Practica3/coleccion.hpp
+++ curso2/EDA/Practica3/coleccion.hpp
@@ -8,120 +8,127 @@
 
 // PREDECLARACION DEL TAD GENERICO agrupacion (inicio INTERFAZ)
 
-const int MAX = 40;
-
-template<typename I, typename V> struct coleccion;
+template<typename I, typename V> struct Coleccion;
 
 // 
-template<typename I, typename V>  void crear(coleccion<I,V>& c);
+template<typename I, typename V>  void crear(Coleccion<I,V>& c);
 
 // 
-template<typename I, typename V>  int tamano(const coleccion<I,V>& c);
+template<typename I, typename V>  int tamano(const Coleccion<I,V>& c);
 
 // 
-template<typename I, typename V>  bool esVacia(const coleccion<I,V>& c);
+template<typename I, typename V>  bool esVacia(const Coleccion<I,V>& c);
 
 // 
-template<typename I, typename V>  bool existe(I id, const coleccion<I,V>& c);
+template<typename I, typename V>  bool existe(I id, const Coleccion<I,V>& c);
 
 // 
-template<typename I, typename V>  bool existeDependiente(I id, coleccion<I,V>& c);
+template<typename I, typename V>  bool existeDependiente(I id, Coleccion<I,V>& c);
 
 // 
-template<typename I, typename V>  bool existeIndependiente(I id, coleccion<I,V>& c);
+template<typename I, typename V>  bool existeIndependiente(I id, Coleccion<I,V>& c);
 
 // 
-template<typename I, typename V>  void anyadirIndependiente(coleccion<I,V>& c, I id, V v);
+template<typename I, typename V>  void anyadirIndependiente(Coleccion<I,V>& c, I id, V v);
 
 // 
-template<typename I, typename V>  void anyadirDependiente(coleccion<I,V>& ccol, I id, V v, I super);
+template<typename I, typename V>  void anyadirDependiente(Coleccion<I,V>& ccol, I id, V v, I super);
 
 // 
-template<typename I, typename V>  void hacerDependiente(coleccion<I,V>& c, I id, I super);
+template<typename I, typename V>  void hacerDependiente(Coleccion<I,V>& c, I id, I super);
 
 // 
-template<typename I, typename V>  void hacerIndependiente(coleccion<I,V>& c, I id );
-
-
-// 
-template<typename I, typename V>  void actualizarVal(coleccion<I,V>& c, I id, V nuevo);
+template<typename I, typename V>  void hacerIndependiente(Coleccion<I,V>& c, I id );
 
 // 
-template<typename I, typename V>  V obtenerVal(I id, const coleccion<I,V>& c);
+template<typename I, typename V>  void actualizarVal(Coleccion<I,V>& c, I id, V nuevo);
 
 // 
-template<typename I, typename V>  I obtenerSupervisor(I id, const coleccion<I,V>& c);
+template<typename I, typename V>  V obtenerVal(I id, const Coleccion<I,V>& c);
 
 // 
-template<typename I, typename V>  int obtenerNumDependientes(I id, const coleccion<I,V>& c);
+template<typename I, typename V>  I obtenerSupervisor(I id, const Coleccion<I,V>& c);
 
 // 
-template<typename I, typename V>  void borrar(I id, coleccion<I,V>& c);
+template<typename I, typename V>  int obtenerNumDependientes(I id, const Coleccion<I,V>& c);
 
 // 
-template<typename I, typename V>  void iniciarIterador(coleccion<I,V>& c);
+template<typename I, typename V>  void borrar(I id, Coleccion<I,V>& c);
 
 // 
-template<typename I, typename V>  bool existeSiguiente(const coleccion<I,V>& c);
+template<typename I, typename V>  void iniciarIterador(Coleccion<I,V>& c);
 
 // 
-template<typename I, typename V>  I siguienteIdent(const coleccion<I,V>& c);
+template<typename I, typename V>  bool existeSiguiente(const Coleccion<I,V>& c);
 
 // 
-template<typename I, typename V>  V siguienteVal(const coleccion<I,V>& c);
+template<typename I, typename V>  I siguienteIdent(const Coleccion<I,V>& c);
 
 // 
-template<typename I, typename V>  bool siguienteDependiente(const coleccion<I,V>& c);
+template<typename I, typename V>  V siguienteVal(const Coleccion<I,V>& c);
 
 // 
-template<typename I, typename V>  I siguienteSuperior(const coleccion<I,V>& c);
+template<typename I, typename V>  bool siguienteDependiente(const Coleccion<I,V>& c);
 
 // 
-template<typename I, typename V>  int siguienteNúmDependientes(const coleccion<I,V>& c);
+template<typename I, typename V>  I siguienteSuperior(const Coleccion<I,V>& c);
 
 // 
-template<typename I, typename V>  void avanza(coleccion<I,V>& c);
+template<typename I, typename V>  int siguienteNumDependientes(const Coleccion<I,V>& c);
 
 // 
-template<typename I, typename V>  I siguienteIdent(coleccion<I,V>& c);
+template<typename I, typename V>  void avanza(Coleccion<I,V>& c);
 
 // 
-template<typename I, typename V>  V siguienteVal(coleccion<I,V>& c);
+template<typename I, typename V>  I siguienteIdent(Coleccion<I,V>& c);
+
+// 
+template<typename I, typename V>  V siguienteVal(Coleccion<I,V>& c);
 
 
 
-// DECLARACION DEL TAD GENERICO coleccion
+// DECLARACION DEL TAD GENERICO Coleccion
 
 template<typename I, typename V>
-struct coleccion{
-   friend template<typename I, typename V>  void crear(coleccion<I,V>& c);
-   friend template<typename I, typename V>  int tamano(const coleccion<I,V>& c);
-   friend template<typename I, typename V>  bool esVacia(const coleccion<I,V>& c);
-   friend template<typename I, typename V>  bool existe(I id, const coleccion<I,V>& c);
-   friend template<typename I, typename V>  bool existeDependiente(I id, coleccion<I,V>& c);
-   friend template<typename I, typename V>  bool existeIndependiente(I id, coleccion<I,V>& c);
-   friend template<typename I, typename V>  void anyadirIndependiente(coleccion<I,V>& c, I id, V v);
-   friend template<typename I, typename V>  void anyadirDependiente(coleccion<I,V>& ccol, I id, V v, I super);
-   friend template<typename I, typename V>  void hacerDependiente(coleccion<I,V>& c, I id, I super);
-   friend template<typename I, typename V>  void hacerIndependiente(coleccion<I,V>& c, I id );
-   friend template<typename I, typename V>  void actualizarVal(coleccion<I,V>& c, I id, V nuevo);
-   friend template<typename I, typename V>  V obtenerVal(I id, const coleccion<I,V>& c);
-   friend template<typename I, typename V>  I obtenerSupervisor(I id, const coleccion<I,V>& c);
-   friend template<typename I, typename V>  int obtenerNumDependientes(I id, const coleccion<I,V>& c);
-   friend template<typename I, typename V>  void borrar(I id, coleccion<I,V>& c);
-   friend template<typename I, typename V>  void iniciarIterador(coleccion<I,V>& c);
-   friend template<typename I, typename V>  bool existeSiguiente(const coleccion<I,V>& c);
-   friend template<typename I, typename V>  I siguienteIdent(const coleccion<I,V>& c);
-   friend template<typename I, typename V>  V siguienteVal(const coleccion<I,V>& c);
-   friend template<typename I, typename V>  bool siguienteDependiente(const coleccion<I,V>& c);
-   friend template<typename I, typename V>  I siguienteSuperior(const coleccion<I,V>& c);
-   friend template<typename I, typename V>  int siguienteNúmDependientes(const coleccion<I,V>& c);
-   friend template<typename I, typename V>  void avanza(coleccion<I,V>& c);
-   friend template<typename I, typename V>  I siguienteIdent(coleccion<I,V>& c);
-   friend template<typename I, typename V>  V siguienteVal(coleccion<I,V>& c);
+struct Coleccion{
+   friend void crear(Coleccion<I,V>& c);
+   friend int tamano(const Coleccion<I,V>& c);
+   friend bool esVacia(const Coleccion<I,V>& c);
+   friend bool existe(I id, const Coleccion<I,V>& c);
+   friend bool existeDependiente(I id, Coleccion<I,V>& c);
+   friend bool existeIndependiente(I id, Coleccion<I,V>& c);
+   friend void anyadirIndependiente(Coleccion<I,V>& c, I id, V v);
+   friend void anyadirDependiente(Coleccion<I,V>& ccol, I id, V v, I super);
+   friend void hacerDependiente(Coleccion<I,V>& c, I id, I super);
+   friend void hacerIndependiente(Coleccion<I,V>& c, I id );
+   friend void actualizarVal(Coleccion<I,V>& c, I id, V nuevo);
+   friend V obtenerVal(I id, const Coleccion<I,V>& c);
+   friend I obtenerSupervisor(I id, const Coleccion<I,V>& c);
+   friend int obtenerNumDependientes(I id, const Coleccion<I,V>& c);
+   friend void borrar(I id, Coleccion<I,V>& c);
+   friend void iniciarIterador(Coleccion<I,V>& c);
+   friend bool existeSiguiente(const Coleccion<I,V>& c);
+   friend I siguienteIdent(const Coleccion<I,V>& c);
+   friend V siguienteVal(const Coleccion<I,V>& c);
+   friend bool siguienteDependiente(const Coleccion<I,V>& c);
+   friend I siguienteSuperior(const Coleccion<I,V>& c);
+   friend int siguienteNumDependientes(const Coleccion<I,V>& c);
+   friend void avanza(Coleccion<I,V>& c);
 
    private:  //declaracion de la representacion interna del tipo
-      // TODO: me he quedado por aqui
+   
+      struct Elemento{
+         I ident;
+         V val;
+         bool esDependiente;
+         I identSup;
+         int numDepend;
+         Elemento* sig;
+      };
+
+      Elemento* primera;
+      int tamano;
+      Elemento* actual;
 
 };
 
